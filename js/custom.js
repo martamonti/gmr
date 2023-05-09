447e878c48b841343e0650e4b9f71f571bbb84c2
Marta Monti <77634550+martamonti@users.noreply.github.com>
2023-05-09 18:16:23 +0200

    recensioni

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,41 @@
+$(function() {
 
-  $(function () {
-
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-
-    // AOS ANIMATION
-    AOS.init({
-      disable: 'mobile',
-      duration: 800,
-      anchorPlacement: 'center-bottom'
-    });
-
-
-    // SMOOTHSCROLL NAVBAR
-    $(function() {
-      $('.navbar a, .hero-text a').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });
+  // MENU
+  $('.navbar-collapse a').on('click', function() {
+    $(".navbar-collapse").collapse('hide');
   });
 
-  
+  // AOS ANIMATION
+  AOS.init({
+    disable: 'mobile',
+    duration: 800,
+    anchorPlacement: 'center-bottom'
+  });
+
+
+  const swiper = new Swiper('.swiper-reviews', {
+
+    // Optional parameters
+
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 3,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+  });
+
+
+  // SMOOTHSCROLL NAVBAR
+  $(function() {
+    $('.navbar a, .hero-text a').on('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+});
